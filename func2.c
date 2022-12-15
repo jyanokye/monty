a02b5aebe4abd9f595e3a9b3583ee2a5e0555dd5
jyanokye <jamesyawanokye17@gmail.com>
2022-12-15 09:48:04 -0800

    kweku

--- func2.c
+++ func2.c
@@ -9,7 +9,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
 	int temp = 0;
-	
+
 	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
@@ -28,7 +28,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = NULL;
-
+	
 	if (*stack == NULL || stack == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
@@ -51,7 +51,7 @@ void _add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = NULL;
 	int sum = 0;
-
+	
 	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
@@ -63,6 +63,7 @@ void _add(stack_t **stack, unsigned int line_number)
 	current->next->n = sum;
 	_pop(stack, line_number);
 }
+
 /**
  * _sub - subtracts the top element of the stack from the second.
  * @stack: Stack list
@@ -72,7 +73,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
 	int sub = 0;
-	
+
 	if (!current || !current->next)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
@@ -83,6 +84,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 	current->next->n = sub;
 	_pop(stack, line_number);
 }
+
 /**
  * _nop - function void
  * @stack: Stack list
