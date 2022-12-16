4f08f224404f8066b28ba6c9f744d1188ab982e4
jyanokye <jamesyawanokye17@gmail.com>
2022-12-15 16:06:35 -0800

    gold

--- fun3.c
+++ fun3.c
@@ -50,3 +50,75 @@ void _mul(stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	current->next->n = mul;
 }
+/**
+ * _mod - computes the rest of the division of the second top element
+ * @stack: Stack.
+ * @line_number: Number of line
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curerent = NULL;
+	int div = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}curerent = *stack;
+	div = curerent->next->n % curerent->n;
+	curerent->next->n = div;_pop(stack, line_number);
+
+}
+
+/**
+ * _pchar - prints the char at the top of the stack, followed by a new line.
+ * @stack: Stack
+ * @line_number: Number of line
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	int num = 0;
+	
+	if (!*stack || !stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	
+	num = (*stack)->n;
+	if (num < 0 || num > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		cleanStack(stack);exit(EXIT_FAILURE);
+	}
+	putchar(num);
+	putchar(10);
+}
+
+/**
+ * _pstr - Print a string.
+ * @stack: stacks
+ * @line_number: Number of line
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+
+	while (current && current->n)
+	{
+		if (current->n < 32 || current->n > 127)
+			break;
+		printf("%c", current->n);
+		current = current->next;
+	}
+	putchar(10);
+}
